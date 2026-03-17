bcc44ef8912e2819c8e20ac748310e60e5918cb3
roaringsurfdev <dev@roaringsurf.net>
2026-03-17 10:07:58 -0700

    updated model->variant_context to clarify meaning and bring code up to date on latest terminology.

--- notebooks/scratch_pad.py
+++ notebooks/scratch_pad.py
@@ -42,19 +42,19 @@ def print_ideal_fourier_spectrum_list(p_list):
 # %% load model and list variants
 family = load_family("modulo_addition_1layer")
 variant = family.get_variant(prime=113, seed=999)
-model = variant.at(9000)
+variant_context = variant.at(9000)
 
 # %% analyze probe
 probe = variant.make_probe([[3, 29]])
-logits, cache = model.run_with_cache(probe)
+logits, cache = variant_context.run_with_cache(probe)
 for param_name, param in cache.items():
     print(param_name, param.shape)
 # %%
-W_E = model.embed.W_E[:-1]
+W_E = variant_context.embed.W_E[:-1]
 print("W_E", W_E.shape)
-W_neur = W_E @ model.blocks[0].attn.W_V @ model.blocks[0].attn.W_O @ model.blocks[0].mlp.W_in
+W_neur = W_E @ variant_context.blocks[0].attn.W_V @ variant_context.blocks[0].attn.W_O @ variant_context.blocks[0].mlp.W_in
 print("W_neur", W_neur.shape)
-W_logit = model.blocks[0].mlp.W_out @ model.unembed.W_U
+W_logit = variant_context.blocks[0].mlp.W_out @ variant_context.unembed.W_U
 print("W_logit", W_logit.shape)
 
 # %% artifacts
